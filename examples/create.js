a40bb17566177dbe6988da242e4433e921edf608
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-28 06:24:24 -0400

    wip

--- examples/create.js
+++ examples/create.js
@@ -1,35 +1,110 @@
-var client = require( '../' ).create({
+var client = module.exports = require( '../' ).create({
   url:      process.env.WORDPRESS_CLIENT_URL,
   username: process.env.WORDPRESS_CLIENT_USERNAME,
   password: process.env.WORDPRESS_CLIENT_PASSWORD
 });
 
-// client.on( '**', debugEvent );
-// client.authenticatedCall
-// client.listMethods( console.log );
+// Create New Post.
+client.insertPost({
+  post_type: 'post',
+  post_status: 'draft',
+  post_title: 'Test Post',
+  post_author: 1,
+  post_excerpt: 'Summary of post',
+  post_content: '<b>Rich-text</b> detail of of post.',
+  post_modified_gmt: new Date(),
+  post_date: new Date(),
+  post_format: 'standard',
+  comment_status: 'closed',
+  ping_status: 'closed',
+  sticky: false,
+  custom_fields: [
+    {
+      key: '_source',
+      value: 'xml-rpc'
+    },
+    {
+      key: 'some-key',
+      value: 'some-value'
+    }
+  ],
+  terms_names: {
+    category: [ 'Awesome Category', 'Another Category' ],
+    post_tag: [ 'Chicago' ]
+  }
+}, insertCallback );
 
-return module.exports = client;
+// Update Existing Post.
+client.insertPost({
+  ID: 1,
+  if_not_modified_since: new Date( ( new Date ).getTime() - ( 60 * 1000 ) ),
+  post_type: 'post',
+  post_status: 'draft',
+  post_title: 'Test Post',
+  post_author: 1,
+  post_excerpt: 'Summary of post',
+  post_content: '<b>Rich-text</b> detail of of post.',
+  post_modified_gmt: new Date(),
+  post_date: new Date(),
+  post_format: 'standard',
+  comment_status: 'closed',
+  ping_status: 'closed',
+  sticky: false,
+  custom_fields: [
+    {
+      key: '_source',
+      value: 'xml-rpc'
+    },
+    {
+      key: 'some-key',
+      value: 'some-value'
+    }
+  ],
+  terms_names: {
+    category: [ 'Awesome Category', 'Another Category' ],
+    post_tag: [ 'Chicago' ]
+  }
+}, insertCallback );
 
-function debugEvent( error, data ) {
-  console.log( "=====event:" + this.event + "=====\n", error || data );
+// Update non-existing post, should fail.
+client.insertPost({
+  ID: 23432324321,
+  if_not_modified_since: new Date( ( new Date ).getTime() - ( 60 * 1000 ) ),
+  post_type: 'post',
+  post_status: 'draft',
+  post_title: 'Test Post',
+  post_author: 1,
+  post_excerpt: 'Summary of post',
+  post_content: '<b>Rich-text</b> detail of of post.',
+  post_modified_gmt: new Date(),
+  post_date: new Date(),
+  post_format: 'standard',
+  comment_status: 'closed',
+  ping_status: 'closed',
+  sticky: false,
+  custom_fields: [
+    {
+      key: '_source',
+      value: 'xml-rpc'
+    },
+    {
+      key: 'some-key',
+      value: 'some-value'
+    }
+  ],
+  terms_names: {
+    category: [ 'Awesome Category', 'Another Category' ],
+    post_tag: [ 'Chicago' ]
+  }
+}, insertCallback );
+
+/**
+ * Insertion Callback.
+ *
+ * @param error
+ * @param data
+ */
+function insertCallback( error, data ) {
+  console.log( require( 'util' ).inspect( error ? error.message : data, { showHidden: true, colors: true, depth: 2 } ) )
 }
-
-// client.on( 'wp.**.success', debugEvent );
-// client.on( 'wp.**.error', debugEvent );
-
-// Post
-client.newPost();
-client.getPost();
-client.editPost();
-client.deletePost();
-
-// Options
-client.getOptions();
-client.getOptions();
-
-// Utility
-client.connect();
-
-// Media
-client.uploadFile();
 
