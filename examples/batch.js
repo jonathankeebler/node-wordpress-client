82b6227f7c8b1222b3b1c1dc6e52e1286dda61ec
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-29 11:30:03 -0400

    updated batch example

--- examples/batch.js
+++ examples/batch.js
@@ -1,3 +1,8 @@
+/**
+ * Get Post Types, Options and Taxonomies in one call.
+ *
+ * @type {*|Suite}
+ */
 var client = module.exports = require( '../' ).create({
   url: process.env.WORDPRESS_CLIENT_URL,
   username: process.env.WORDPRESS_CLIENT_USERNAME,
@@ -5,10 +10,20 @@ var client = module.exports = require( '../' ).create({
 });
 
 var _items = [
-  { methodName: 'wp.getMediaItem', params: [ client.get( 'blog' ), client.get( "username" ), client.get( "password" ), 251 ] },
-  { methodName: 'wp.getMediaItem', params: [ client.get( 'blog' ), client.get( "username" ), client.get( "password" ), 250 ] }
+  {
+    methodName: 'wp.getPostTypes',
+    params: [ client.get( 'blog' ), client.get( "username" ), client.get( "password" ) ]
+  },
+  {
+    methodName: 'wp.getOptions',
+    params: [ client.get( 'blog' ), client.get( "username" ), client.get( "password" ), [ 'large_size_w', 'large_size_h' ]  ]
+  },
+  {
+    methodName: 'wp.getTaxonomies',
+    params: [ client.get( 'blog' ), client.get( "username" ), client.get( "password" ) ]
+  }
 ];
 
 client.multiCall( _items, function(err, results) {
-  console.log( require( 'util' ).inspect( results, { showHidden: false, colors: true, depth: 1 } ) )
+  console.log( require( 'util' ).inspect( results, { showHidden: false, colors: true, depth: 3 } ) )
 });
